65f42d6f3733bb5e01413c3a21febda4c19330b5
Alfredo Silva <a.alfredops@gmail.com>
2023-10-19 12:16:12 -0400

    100 Days of Python - Day 7 - Project Hangman

--- Hangman/main.py
+++ Hangman/main.py
@@ -1,20 +1,25 @@
 import random
 
 word_list = ["alfredo", "carolina", "Marcely"]
+print("Welcome to Hangman.\nYou have to discover what word it is:")
+word = random.choice(word_list).lower()
 
-# Remember to use random.choice - Should be a cleaner sintaxe here.
-word = word_list[random.randint(0, len(word_list) - 1)]
+displa_list = ['_'] * len(word)
+life = 4
 
-empty_list = []
-for char in range(len(word)):
-    empty_list.append("_")
+while life > 0 and '_' in displa_list:
+    letter = input(f"You have {life} lives. Make a guess and discover a letter: ").lower()
 
-letter = input("Guess a letter.")
+    if letter not in word:
+        life -= 1
+    else:
+        for i, char in enumerate(word):
+            if char == letter:
+                displa_list[i] = letter
 
-for i, char in enumerate(word):
-    if char.lower() == letter.lower():
-        empty_list[i] = letter.lower()
+    print(''.join(displa_list))
 
-print(empty_list)
-
-
+if "_" not in displa_list:
+    print("Congratulations! You won!")
+else:
+    print("Sorry, you lost. The word was", word)
